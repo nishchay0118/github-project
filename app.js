7ba6271b08a0e22a7f9f7bb3ea39c93aed559c5e
Ubuntu <ubuntu@ip-172-31-22-62.ec2.internal>
2026-06-02 09:36:29 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("CI/CD Pipeline Working");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "healthy"
+  });
+});
